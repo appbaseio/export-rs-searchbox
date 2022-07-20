fa45cf628b5c2445779fad283cb27f16771a65a9
Mohd Ashraf <57627350+mohdashraf010897@users.noreply.github.com>
2022-07-20 20:26:12 +0530

    feat: add SearchBox component with dummy searchbox preferences

--- src/index.js
+++ src/index.js
@@ -1,13 +1,101 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import { isIdAvailble } from "./utils/helper";
+import { ReactiveBase, SearchBox } from "@appbaseio/reactivesearch";
 
-const renderById = (id) => {
+import "./index.css";
+import {
+  // fetchSearchBoxPreferences,
+  getPropsById,
+  isIdAvailble,
+} from "./utils/helper";
+
+const renderById = async (id) => {
   const container = isIdAvailble(id);
+
+  const { searchBoxId, clusterUrl, credentials } = getPropsById(id);
+
+  const {
+    searchbox: {
+      popular,
+      recent,
+      featured: { design, layout },
+    },
+  } = {
+    id: "document-search",
+    enabled: true,
+    hidden: false,
+    searchbox: {
+      popular: {
+        size: 5,
+        index: "good-books-ds",
+        minChars: 3,
+        minCount: 1,
+        customEvents: {
+          platform: "mac",
+        },
+        sectionLabel: "Recent",
+      },
+      recent: {
+        size: 5,
+        index: "good-books-ds",
+        minHits: 1,
+        minChars: 3,
+        customEvents: {
+          platform: "mac",
+        },
+        sectionLabel: "Recent",
+      },
+      featured: {
+        design: {
+          primaryColor: "red",
+          theme: "dark",
+        },
+        layout: {
+          maxSuggestionsPerSection: 3,
+          sectionsOrder: ["examples", "docs"],
+        },
+      },
+    },
+    created_at: 1657667251,
+    updated_at: 1657667259,
+  };
+  // await fetchSearchBoxPreferences({
+  //     url: clusterUrl,
+  //     credentials,
+  //     searchBoxId,
+  //   })
+
   const root = ReactDOM.createRoot(document.getElementById(id));
+
   if (container) {
-    root.render(<div>HEY</div>, root);
+    root.render(
+      <ReactiveBase
+        url={clusterUrl}
+        credentials={credentials}
+        enableAppbase
+        app="test"
+      >
+        <SearchBox
+          componentId={searchBoxId}
+          enablePopularSuggestions
+          enableRecentSuggestions
+          enableFeaturedSuggestions
+          popularSuggestionsConfig={{
+            ...popular,
+          }}
+          recentSuggestionsConfig={{
+            ...recent,
+          }}
+          featuredSuggestionsConfig={{
+            featuredSuggestionsGroupId: id, // # mandatory
+            maxSuggestionsPerSection: layout.maxSuggestionsPerSection,
+            sectionsOrder: layout.sectionsOrder,
+          }}
+          themePreset={design.theme}
+        />
+      </ReactiveBase>,
+      root
+    );
   }
 };
 
--- src/utils/helper.js
+++ src/utils/helper.js
@@ -4,13 +4,31 @@ export const getPropsById = (id) => {
   const container = isIdAvailble(id);
   if (container) {
     return {
-      widgetId: container.getAttribute("widget-id"),
-      currentProduct: container.getAttribute("current-product"),
-      isOpen: container.getAttribute("isOpen") === "true",
-      openAsPage: container.getAttribute("openaspage") === "true",
-      isPreview: container.getAttribute("isPreview") === "true",
-      disableSearchText: container.getAttribute("disableSearchText") === "true",
+      searchBoxId: container.getAttribute("searchbox-id"),
+      clusterUrl: container.getAttribute("cluster-url"),
+      credentials: container.getAttribute("credentials"),
     };
   }
   return null;
+};
+
+export const fetchSearchBoxPreferences = ({
+  url,
+  credentials,
+  searchBoxId,
+}) => {
+  let myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  myHeaders.append("Authorization", "Basic " + btoa(credentials));
+  const requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+  };
+
+  return fetch(`${url}/_uibuilder/searchbox/${searchBoxId}`, requestOptions)
+    .then((response) => response.json())
+    .catch((error) => {
+      console.log("error", error);
+      return error;
+    });
 };
